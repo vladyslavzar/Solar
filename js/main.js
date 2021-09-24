30a25eef4b197643ba31017f03eebd7a6148168f
vladyslavzar <82956846+vladyslavzar@users.noreply.github.com>
2021-09-24 21:03:40 +0300

    Solar

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('.greentarifcontainerfirstslider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    });
+    $('.clientscontainerfirstslider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    });
+});
